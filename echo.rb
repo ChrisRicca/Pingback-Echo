673d4ac99846a96ee93742f3a513412c7abcceb6
Chris Ricca <chris@dropio.com>
2010-03-30 11:33:22 -0400

    fix to remove stray param

--- echo.rb
+++ echo.rb
@@ -16,7 +16,7 @@ end
 DataMapper.auto_upgrade!
 
 post '/:url_h3879h23' do
-  Ping.create :url => params[:url_h3879h23], :contents => (params).delete_if{|k,v| k == :url_h3879h23}.inspect
+  Ping.create :url => params[:url_h3879h23], :contents => (params).delete_if{|k,v| k == "url_h3879h23"}.inspect
 end
 
 get '/:url' do
