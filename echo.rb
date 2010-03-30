c01ef98deb0b0091f2a82221a13790cb968369e5
Chris Ricca <chris@dropio.com>
2010-03-30 12:19:25 -0400

    use javascript for the example

--- echo.rb
+++ echo.rb
@@ -15,16 +15,17 @@ class Ping
 end
 DataMapper.auto_upgrade!
 
-get '/' do
-  random = rand(10000000000).to_s(16)
-  link = "http://echo.heroku.com/#{random}"
-  "Try it out: <ol><li>curl #{link} -d \"foo=bar&awe=some\"</li><li><a href='#{link}'>#{link}</a></li>"
-end
-
 post '/:url_h3879h23' do
   Ping.create :url => params[:url_h3879h23], :contents => (params).delete_if{|k,v| k == "url_h3879h23"}.inspect
 end
 
 get '/:url' do
   Ping.all(:url => params[:url]).collect{|ping| "<div>#{ping.created_at}: #{ping.contents}</div>"}.join("<br/>")
+end
+
+# Welcome Page
+get '/' do
+  random = rand(10000000000).to_s(16)
+  link = "http://<script>document.write(document.location.host)</script>/#{random}"
+  "Try it out: <ol><li>curl #{link} -d \"foo=bar&awe=some\"</li><li>curl #{link}</li>"
 end
