3316e8e0e38308da716b75d46e861065ce1199e9
Chris Ricca <chris@dropio.com>
2010-03-30 09:51:33 -0400

    initial commit

---
+++ echo.rb
@@ -0,0 +1,24 @@
+require 'rubygems'
+require 'sinatra'
+require 'dm-core'
+require 'dm-timestamps'
+require 'pp'
+
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/development.sqlite3")
+
+class Ping
+  include DataMapper::Resource
+  
+  property :id, Serial
+  property :url, String
+  property :contents, Text, :lazy => false
+end
+DataMapper.auto_upgrade!
+
+post '/:url' do
+  Ping.create(:url => params[:url], :contents => pp(request))
+end
+
+get '/:url' do
+  Ping.all(:url => params[:url]).collect{|ping| "<div>#{ping.contents}</div>"}.join("")
+end
